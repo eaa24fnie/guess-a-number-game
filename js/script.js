ef2f0caa9114bfb7b11ed797d732f0d2665c7b60
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-01-29 20:55:23 +0100

    DONE

--- js/script.js
+++ js/script.js
@@ -1,2 +1,62 @@
 "use strict";
 
+let targetNum = Math.floor(Math.random() * 101);
+
+function checkGuess() {
+    let guess = parseInt(document.getElementById("guessInput").value, 10);
+    let message = document.getElementById("message");
+    
+    if (isNaN(guess)) {
+        message.textContent = "Gæt et gyldigt tal.";
+        return;
+    }
+    
+    if (guess < targetNum) {
+        message.textContent = "FOR LAVT!! GÆT IGEN!!!";
+    } else if (guess > targetNum) {
+        message.textContent = "ALT FOR HØJT! GÆT IGEN!!!";
+    } else {
+        message.textContent = "GZ! Du vandt!";
+        alert("Du vandt! Det rigtige tal var " + targetNum);
+        targetNum = Math.floor(Math.random() * 101); // Ny runde
+    }
+}
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
